c85b59fec2b4eee062dd02efd63382c8c6937fe6
KuaqSon <qson.ng@gmail.com>
2023-06-02 12:42:03 +0700

    feat(EmptyPageLayout.tsx): create a reusable layout component for empty pages with SEO metadata and a container for content

---
+++ src/components/EmptyPageLayout.tsx
@@ -0,0 +1,40 @@
+import { Container, Flex } from '@chakra-ui/react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+export default function EmptyPageLayout(props: any) {
+  const { children, ...customMeta } = props;
+  const router = useRouter();
+  const meta = {
+    title: 'Son Nguyen',
+    description: 'Software Engineering',
+    image: 'https://nqson.com/static/nqson-banner.png',
+    type: 'website',
+    ...customMeta,
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`https://nqson.com${router.asPath}`} />
+        <link rel="canonical" href={`https://nqson.com${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Son Nguyen" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@quangsonnguyen2" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+      </Head>
+      <Container m="auto" maxW="container.md">
+        {children}
+      </Container>
+    </>
+  );
+}
